054daeb24725822fa122ad743ab187741682eb4e
Angus Hay <angushay42@gmail.com>
2026-04-28 14:58:24 +0100

    Testing is going well, formalising is also going well, so well that I have forgotten to committ anything yet.

--- app/server/server.py
+++ app/server/server.py
@@ -18,20 +18,21 @@ class UARTException(BaseException):
 class UART:
     port_name = ""
     file_name = ""
-    stream: serial.Serial
+    stream: serial.Serial   = None
+    manager: 'BytesManager' = None
 
-    byte    = 0
-    half    = 1
-    word    = 2
-    double  = 4
-    signed  = 5
+    byte        = 0
+    half        = 1
+    word        = 2
+    double      = 4
+    floating    = 5
+    signed      = 6
 
     def __init__(self, timeout: float, test: bool = False):
         self.timeout = timeout
         if test:
             return
         
-
         ports = [x for x in os.listdir("/dev") if x.startswith("tty.") and "usbmodem" in x]
         assert ports
         self.port_name = f"/dev/{ports[0]}"
@@ -44,48 +45,9 @@ class UART:
                 timeout=self.timeout
             )
         except SerialException as e:
-            
-            raise UARTException(*e)
-
-    def convert_to_bytes(self, data: int, size = 2) -> bytearray:
-        if not size in [1,2,4,8]:
-            raise UARTException(f"Invalid data size: {size}. Must be [1,2,4,8].")
-    
-        b = bytearray()
+            raise UARTException(e)  # todo
         
-        mask = 0
-        if data < 0:
-            mask |= (1 << self.signed)
-        mask |= 1 << (size // 2)
-
-        b.append(mask)
-
-        b += data.to_bytes(size, byteorder='little', signed=data<0)
-        return b
-
-    @staticmethod
-    def get_size(num: int) -> int:
-        # todo i hate this
-        if num < 0:
-            num *= -1
-        if num > (1 << 32):
-            return 8
-        if num > (1 << 16):
-            return 4
-        if num > (1 << 8):
-            return 2
-        return 1
-        
-        # get the smallest word size that fits the num
-        
-        # just invert negative numbers?
-        # temp = num
-        # size = 0
-        # while (temp):
-        #     temp >>= 8
-        #     size += 1
-
-        # return size
+        self.manager = BytesManager()
 
     def validate_flag(self, flag: int) -> None:
         """
@@ -98,19 +60,22 @@ class UART:
             temp >>= 1
         # 1 or 2 bits are set
         if not (0 < num_bits <= 2):
-            raise UARTException(f"Invalid flag {bin(flag):80} with {num_bits} bits; should have 2.")
+            raise UARTException(f"Invalid flag {bin(flag):8} with {num_bits} bits; should have 2.")
         
         if (flag >> (self.signed+1)) != 0:
-            raise UARTException(f"Invalid flag {bin(flag):80} with invalid bits {flag >> self.signed}")
+            raise UARTException(f"Invalid flag {bin(flag):8} with invalid bits {bin(flag >> self.signed):8}")
         
 
     def send(self, data):
+        # todo validate length is 0-255
         # convert data to format 
         # write to port
         pass
 
     def recv(self) -> tuple[list[int], str | None]:
         """Read flag unsigned byte (flag), length unsigned byte (n) and return n bytes (data) and string, if possible."""
+
+        # todo FLOATING POINT NUMBERS
         b = self.stream.read(1)
         if not b:
             raise UARTException("Flag byte not found.")
@@ -124,12 +89,13 @@ class UART:
             raise UARTException("Length byte not found.")
         len = int.from_bytes(b, 'little', signed=False)
         
+        
         # extract meta data from flags: Are bytes signed, how big are they?
         signed = bool((flag >> self.signed) & 1)
         size = flag & ((1 << self.signed) - 1)
 
         data = []
-        for i in range(1, len // size):
+        for i in range(len // size):
             # read size bytes
             b = self.stream.read(size)
             if not b:
@@ -138,12 +104,57 @@ class UART:
             data.append(int.from_bytes(b, 'little', signed=signed))
 
         # probably a character
-        if not signed and size == self.byte:
+        if not signed and size == (1 << self.byte):
             s = "".join([chr(x) for x in data])
             return (data, s)
         
         return (data, None)
         
+class BytesManager:
+
+    @classmethod
+    def _convert_many(cls, data, size, signed):
+
+    
+
+    @classmethod
+    def _convert_once(cls, data, size, signed):
+
+    
+
+    @classmethod
+    def convert_to_bytes(cls, data: int | list, size = 2, signed: bool = False) -> bytearray:
+        if not size in [1,2,4,8]:
+            raise UARTException(f"Invalid data size: {size}. Must be [1,2,4,8].")
+    
+        if isinstance(data, list):
+            
+            raise UARTException(f"Empty list.")
+        
+        if isinstance(data, int):
+            signed = data < 0
+
+        b = bytearray()
+        try:
+            b += data.to_bytes(size, byteorder='little', signed=signed)
+        except OverflowError as e:
+            raise UARTException(f"Incompatible size: {size} for data {data}.")
+        return b
+    
+
+    @classmethod
+    def get_size(cls, num: int) -> int:
+        # todo i hate this
+        if num < 0:
+            num *= -1
+        if num > (1 << 32):
+            return 8
+        if num > (1 << 16):
+            return 4
+        if num > (1 << 8):
+            return 2
+        return 1
+
 
 class MockSerial:
     io: BytesIO
@@ -154,10 +165,10 @@ class MockSerial:
         # self.size = size  # todo
         self.io = BytesIO()
 
-    def read(self, n: int = 1):
+    def read(self, n: int = 1) -> bytes | bytearray:
         new = self.io.seek(self.idx)
         data = self.io.read(n)
-        self.idx = new    # read might do this already?
+        self.idx = self.io.tell()
         return data
 
     def write(self, b): # todo typing bytes-like
--- app/server/test_server.py
+++ app/server/test_server.py
@@ -1,10 +1,8 @@
-from unittest import TestCase
-from io import BytesIO
-import json
-import math
+import unittest
+import random
 
 # test obj
-from server import UART, UARTException, MockSerial
+from server import UART, UARTException, MockSerial, BytesManager
 
 
 """
@@ -27,43 +25,106 @@ so that it uses a test buffer.
 
 """
 
+class TestMockSerial(unittest.TestCase):
+    def test_read(self):
+        data = [12, 34, 89, 101]
+        self.stream.io.write(bytearray(data))
+        ans = []
+        for i in range(len(data)):
+            ans.append(int.from_bytes(self.stream.read(1), 'little')) 
+        self.assertEqual(data,ans)
 
 
-class TestServer(TestCase):
-    server: UART = None
+    def test_write(self):
+        pass
+    
+    def setUp(self):
+        super().setUp()
+        self.stream = MockSerial()
 
-    def test_convert_to_bytes(self):
-        # init vars
-        data = int(0x6e4fa)
-        size = 4
-        
+class TestBytesManager(unittest.TestCase):
+    manager: BytesManager = None
+
+    def test_convert_to_bytes(self):      
         # test exceptions
+        ## invalid byte sizes
         with self.assertRaises(UARTException):
-            self.server.convert_to_bytes(data, 5)
+            self.manager.convert_to_bytes(1, 5)
         with self.assertRaises(UARTException):
-            self.server.convert_to_bytes(data, -1)
+            self.manager.convert_to_bytes(1, -1)
         with self.assertRaises(UARTException):
-            self.server.convert_to_bytes(data, 3)
+            self.manager.convert_to_bytes(1, 3)
+
+        ## incompatible byte sizes
+        with self.assertRaises(UARTException):
+            data = int(0x6e4fa)
+            self.manager.convert_to_bytes(data, 1)
+
+        with self.assertRaises(UARTException):
+            data = -3930300002
+            self.manager.convert_to_bytes(data, 4)
+
     
         # test values
         ## unsigned
-        ans = bytearray([1 << self.server.word])
-        ans += data.to_bytes(4, 'little')
+        data = int(0x6e4fa)
+        size = 4
 
-        self.assertEqual(self.server.convert_to_bytes(data, size), ans)
+        ans = data.to_bytes(4, 'little', signed=False)
+        self.assertEqual(self.manager.convert_to_bytes(data, size), ans)
+
+        data = 14329
+        size = 2
+
+        ans = data.to_bytes(2, 'little', signed=False)
+        self.assertEqual(self.manager.convert_to_bytes(data, size), ans)
 
         ## signed
+        data = -30901
+        size = 4
+
+        ans = data.to_bytes(4, 'little', signed=True)
+        self.assertEqual(self.manager.convert_to_bytes(data, size), ans)
+
+        data = -404040404
+        ans = data.to_bytes(4, 'little', signed=True)
+        self.assertEqual(self.manager.convert_to_bytes(data, size), ans)
+
+        # test list of ints
+        ## exceptions 
+
+        ### empty list
+        with self.assertRaises(UARTException):
+            self.manager.convert_to_bytes([]) 
+
+        ### inconsistent sizes
+        with self.assertRaises(UARTException):
+            self.manager.convert_to_bytes([1, 1 << 32])
+
+
+
+        
+
 
     def test_get_size(self):
-        self.assertEqual(self.server.get_size(4), 1)
+        self.assertEqual(self.manager.get_size(4), 1)
 
-        self.assertEqual(self.server.get_size(254), 1)
+        self.assertEqual(self.manager.get_size(254), 1)
 
-        self.assertEqual(self.server.get_size(6550303), 4)
+        self.assertEqual(self.manager.get_size(6550303), 4)
 
-        self.assertEqual(self.server.get_size(-1), 1)
+        self.assertEqual(self.manager.get_size(-1), 1)
 
-        self.assertEqual(self.server.get_size(-257), 2)
+        self.assertEqual(self.manager.get_size(-257), 2)
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.manager = BytesManager()
+
+
+class TestUART(unittest.TestCase):
+    server: UART = None
 
     def test_send(self):
         pass
@@ -77,39 +138,90 @@ class TestServer(TestCase):
         
         # invalid flag
         ## invalid bits
-        flag = 1 << 6
-        flag |= 1 << 7      
+        flag = 1 << (UART.signed + 1)
+        flag |= 1 << (UART.signed + 2)
 
         with self.assertRaises(UARTException, msg=f"Invalid bits set."):
             self.server.stream.write(bytearray([flag, 1, 3]))
             self.server.recv()
 
         ## bit count either 1 or 2.
-
-        with self.assertRaises(UARTException, msg="Invalid bit count"):
-            self.server.stream.write(bytearray([0, 1, 3]))
+        input = [0, 1, 3]
+        with self.assertRaises(UARTException, msg=f"Invalid bit count with input: {input}"):
+            self.server.stream.write(bytearray())
             self.server.recv()
 
+        input = [7, 1, 3]
+        with self.assertRaises(UARTException, msg=f"Invalid bit count with input: {input}"):
+            self.server.stream.write(bytearray(input))
+            self.server.recv()
 
+        # test data integrity
+        ## single bytes
+        data = [2, 3, 4, 5, 90, 140, 57, 30]
+        flag = 1 << UART.byte
 
-    def setUp(self):
-        return super().setUp()
-    
-    def tearDown(self):
-        return super().tearDown()
-    
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
+        self.server.stream.write(bytearray([flag, len(data)] + data))
+        values, s = self.server.recv()
         
+        # unsigned bytes can be converted to string
+        self.assertIsNotNone(s)
+        self.assertEqual(data, values)
 
+        ## words (4 bytes)
+        data = [32, 5454, 11010, 20202, 34040, 54400, (1 << 30) + 435]
+        flag = 1 << UART.word
+        
+        # byte array only accepts 0-255, must be split up first.
+        d_bytes = bytearray()
+        for d in data:
+            d_bytes += d.to_bytes(1 << UART.word, 'little')
+
+
+        self.server.stream.write(bytearray([flag, len(data)]) + d_bytes)
+        values, s = self.server.recv()
+
+        self.assertIsNone(s)
+        self.assertEqual(data, values)
+
+
+        ## double words (8 bytes)
+        data = []
+        flag = 1 << UART.double
+        for _ in range(8):
+            data.append(random.randint(1 << 32, (1 << 64) - 1))
+
+        d_bytes = bytearray()
+        for d in data:
+            d_bytes += d.to_bytes(1 << UART.double, 'little')
+
+        self.server.stream.write(bytearray([flag, len(data)]) + d_bytes)
+        values, s = self.server.recv()
+
+        self.assertIsNone(s)
+        self.assertEqual(data, values)
+
+        # BUG don't know how big floats are.
+        ## floats   #todo
+
+
+        # todo
+        ## double floating point words (8 bytes)
+        data = [1.4322234, 103.3030, 100000.20 ,203030.39393, 0.991]
+    
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         # timeout of 5
         cls.server = UART(5, True)
         cls.server.stream = MockSerial()
+        
 
 
 if __name__ == "__main__":
-    pass# todo
+    suite = unittest.suite.TestSuite()
+
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestBytesManager))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestMockSerial))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestUART))
+    unittest.TextTestRunner(verbosity=2, failfast=True).run(suite)
