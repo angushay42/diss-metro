7173bd9d858e281ced94beda84962d7d547ca4e9
Angus Hay <angushay42@gmail.com>
2026-05-13 18:51:25 +0100

    took some more samples using the precise=false option of get_time and the precise stamps clearly have a smoother resolution.

--- app/visualisation/visualise_sample.py
+++ app/visualisation/visualise_sample.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
 import numpy as np
 import json
 import os
@@ -16,18 +18,30 @@ for sf in sound_files:
     with open(DATA_DIR / sf, "r") as f:
         raw = json.load(f)
         x_points = [x[0] for x in raw]
-        y_points = [x[1] for x in raw]
+        y_points = [abs(x[1]) for x in raw]
         data[key]["x"] = x_points
         data[key]["y"] = y_points
 
 # params are rows and cols of subplots
-rows, cols = 2, 2
+# todo unsure if this is expandable but works for 8 ?
+rows, cols = 2, 4
 fig, axs = plt.subplots(rows, cols)
-
+fig: Figure
+axs: Axes
 idx = 0
-for title, points in data.items():
-    i, j = idx // rows, idx % cols
-    axs[i, j].plot(points["x"], points["y"])
+
+def my_key(pair):
+    x = pair[0]
+    parts = x.split("-")
+    ans =  parts[-1] + "".join(parts[0:-1])
+    return ans
+
+
+# sort them into precise and imprecise
+sorted_items = sorted(data.items(), key=my_key)
+for title, points in sorted_items:
+    i, j = idx // cols, idx % cols
+    axs[i, j].plot(points["x"], points["y"], )
     axs[i, j].set_title(title)
     axs[i, j].set_ylim(-4096, 4095)
     idx += 1
