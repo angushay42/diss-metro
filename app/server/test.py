aa6845c40d8765c0f3c1ecedd48ad85a61c94df5
Angus Hay <angushay42@gmail.com>
2026-05-19 13:05:10 +0100

    Added some more tests to see what values would be produced for quavers too. I think 2048 will be the new prescaler, as it is close to the minimum allowable and a nice power of 2.

--- app/server/test.py
+++ app/server/test.py
@@ -1,9 +1,12 @@
+apb = 84_000_000
+max_psc = 65535
+
+
 def validate(psc):
     return 0 <= psc <= 65535 
 
-def main():
-    apb = 84_000_000
-    max_psc = 65535
+def get_min_psc():
+    
 
     # frequency of led pulse
     fpulse = 1 / (100 / 1000)
@@ -24,9 +27,32 @@ def main():
             break
         mul += 0.01
 
-    print(f"clk frequency: {clk}, psc: {psc}")
-    print(f"pscs: LED: {clk / fpulse}, min: {clk / min_bpm}, max: {clk / max_bpm}")
+    # print(f"clk frequency: {clk}, psc: {psc}")
+    # print(f"pscs: LED: {clk / fpulse}, min: {clk / min_bpm}, max: {clk / max_bpm}")
+    return psc
+
+def main():
+    # get the minimum clock division factor that allows for all desired frequencies
+    # i.e. pulse (10Hz), minimum bpm (0.66Hz), maximum bpm (3.66Hz)
+    # this is returning 1966 for now.
+    min_psc = get_min_psc()
     
+    min_psc = 2500
+
+    clk = apb / min_psc
+    print(f"{apb / (10**6):.3f}MHz / {min_psc:.3f} = {clk:.3f}Hz", end="\n")
+    print(f"LED: {clk / (1 / (100 / 1000))}\n")
+
+    bpms = [50, 60, 80, 100, 135, 180, 220]
+    for bpm in bpms:
+        # convert to Hz
+        f = bpm / 60
+        print(f"Prescaler values for bpm: {bpm:.3f}")
+        print(f"Crotchets: {clk / f:.3f}")
+        print(f"Quavers: {clk / (f* 2):.3f}")
+        print()
+
+    pass
 
 if __name__ == "__main__":
     main()
